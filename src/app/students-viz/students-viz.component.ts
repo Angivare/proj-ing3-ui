effd45b8325f5c16e02bfca756b39f51d2568ffa
Hakan Keskin <angivare-contact@yahoo.fr>
2019-11-28 14:06:06 +0100

    Implemented user stats page

--- src/app/students-viz/students-viz.component.ts
+++ src/app/students-viz/students-viz.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { UserModel } from '../services/user-data.model';
+import { UserDataService } from '../services/user-data.service';
+import { AuthenticationService } from '../services/authentication.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-students-viz',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentsVizComponent implements OnInit {
 
-  constructor() { }
+  users: UserModel[];
+
+  @ViewChild('authorizedOptionChart', {static: true})
+  authorizedOptionCanvas: ElementRef;
+  authorizedOptionsChart: Chart;
+
+  constructor(
+    public userService: UserDataService,
+    public authService: AuthenticationService
+  ) { }
 
   ngOnInit() {
+    this.userService.getUsers(
+      this.authService.data.authorized_tags
+    ).then(
+      users => this.users = users
+    );
+
+    this.authorizedOptionsChart = new Chart(this.authorizedOptionCanvas
+                                                        .nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['icc', 'ia'],
+        datasets: [{
+          label: 'Importés dans l\'appli',
+          data: [200, 300],
+          backgroundColor: 'green'
+        }, {
+          label: 'Ayant consenti à la collecte',
+          data: [60, 75],
+          backgroundColor: 'magenta'
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
   }
 
 }
