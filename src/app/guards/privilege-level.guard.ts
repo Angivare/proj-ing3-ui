eb4ba9f3d2eae40a5476e242c95f4e3419d149e5
Hakan Keskin <hakan.keskin@eisti.fr>
2019-12-04 16:35:30 +0100

    Fixed buffy double redirection

--- src/app/guards/privilege-level.guard.ts
+++ src/app/guards/privilege-level.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { ActivationResponse } from './types';
 import { AuthenticationService } from '../services/authentication.service';
 
@@ -9,17 +9,18 @@ import { AuthenticationService } from '../services/authentication.service';
 })
 export class PrivilegeLevelGuard implements CanActivate {
   constructor(
-    public authService: AuthenticationService
+    public authService: AuthenticationService,
+    public router: Router
   ) {}
 
   canActivate(next: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): ActivationResponse
   {
-      const opts = next.data;
-      const level = this.authService.getPrivilegeLevel();
+    const opts = next.data;
+    const level = this.authService.getPrivilegeLevel();
 
-      if(opts.level && !opts.level.includes(level))
-      return false;
+    if(opts.level && !opts.level.includes(level))
+      this.router.navigate(['/']);
     
     return true;
   }
